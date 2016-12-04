e89d933e22e0764686c2a3fa873cd1b19334cfd8
tbergq <trond_bergquist@hotmail.com>
2016-12-04 15:18:44 +0100

    Set prod url

--- src/client/app/utils/transportation.js
+++ src/client/app/utils/transportation.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:9005';
-//'https://tv-helper-backend.herokuapp.com';
+const BASE_URL = //'http://localhost:9005';
+'https://tv-helper-backend.herokuapp.com';
 
 class Transportation {
 
