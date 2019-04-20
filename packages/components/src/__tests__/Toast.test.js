0cd84ed518711694c0ee99d271f4ae9c6b6a406c
Trond Bergquist <trond_bergquist@hotmail.com>
2019-04-20 13:57:18 +0200

    Components: Add toast component
    Use Toast component in LoginForm

---
+++ packages/components/src/__tests__/Toast.test.js
@@ -0,0 +1,28 @@
+// @flow
+
+import * as React from 'react';
+import { create } from 'react-test-renderer';
+
+import Toast from '../Toast';
+
+jest.useFakeTimers();
+
+it('renders', () => {
+  expect(create(<Toast message="test" onHide={jest.fn()} />))
+    .toMatchInlineSnapshot(`
+    <div
+      className="sc-bdVaJa dBwOnI"
+    >
+      test
+    </div>
+  `);
+});
+
+it('calls onHide', () => {
+  const onHide = jest.fn();
+  const wrapper = create(<Toast message="test" onHide={onHide} />);
+  wrapper.update();
+  jest.runAllTimers();
+
+  expect(onHide).toHaveBeenCalledWith();
+});
