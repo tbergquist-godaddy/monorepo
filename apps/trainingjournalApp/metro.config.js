124b5a8ac63e6552e270007e9e975979354d00bf
Trond Bergquist <trond_bergquist@hotmail.com>
2019-07-06 08:35:48 +0200

    Add new react-native app

---
+++ apps/trainingjournalApp/metro.config.js
@@ -0,0 +1,20 @@
+// @flow
+
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+module.exports = {
+  transformer: {
+    // eslint-disable-next-line require-await
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
