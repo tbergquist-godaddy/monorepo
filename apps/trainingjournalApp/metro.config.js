c4cb04773ed04a972b61d30e42afb6a13645da86
Trond Bergquist <trond_bergquist@hotmail.com>
2019-08-04 10:10:41 +0200

    Remove nohoisting of react-native

--- apps/trainingjournalApp/metro.config.js
+++ apps/trainingjournalApp/metro.config.js
@@ -8,21 +8,14 @@
  */
 
 const path = require('path');
-const blacklist = require('metro-config/src/defaults/blacklist');
 
 const cwd = path.resolve(__dirname);
 
 module.exports = {
-  resolver: {
-    blacklistRE: blacklist([
-      /node_modules\/.*\/node_modules\/react-native\/.*/,
-      /packages\/.*\/node_modules\/react-native\/.*/,
-    ]),
-  },
   watchFolders: [
     cwd,
-    path.resolve(cwd, '../../packages'),
-    path.resolve(cwd, '../../node_modules'),
+    path.resolve(cwd, '..', '..', 'packages'),
+    path.resolve(cwd, '..', '..', 'node_modules'),
     path.resolve(cwd, '..', '..', 'apps', 'trainingjournal-core'),
   ],
   transformer: {
