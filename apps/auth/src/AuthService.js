e1f32f69d4211b5cc6590623217ae9d8b5f0d5c9
Trond Bergquist <trond_bergquist@hotmail.com>
2019-08-11 12:45:44 +0200

    auth: Add tvhelper login

--- apps/auth/src/AuthService.js
+++ apps/auth/src/AuthService.js
@@ -1,8 +1,45 @@
 // @flow
 
+import { UserRepository } from '@tbergq/tvhelper-persistence';
+import { verify } from 'password-hash';
+import jwt from 'jsonwebtoken';
+
 import { LoginReply } from './__generated__/auth_pb';
 
-export function login(call: $FlowFixMe, callback: $FlowFixMe) {
-  const reply = new LoginReply(['', 'token', true]);
-  callback(null, reply);
+const { JWT_SECRET } = process.env;
+
+function getRepository(app: number) {
+  switch (app) {
+    case 0:
+      return UserRepository;
+    default:
+      throw new Error('Unknown app');
+  }
+}
+
+function signToken(user: {| +id: string, +username: string, +app: number |}) {
+  return jwt.sign(user, JWT_SECRET, {
+    expiresIn: '1y',
+    issuer: 'tbergq-graphql.now.sh',
+  });
+}
+
+export async function login(call: $FlowFixMe, callback: $FlowFixMe) {
+  const request = call.request.toObject();
+  const repository = getRepository(request.app);
+  const user = await repository.findUser(request.username);
+
+  const isCorrect = verify(request.password, user.password);
+
+  if (!isCorrect) {
+    callback(null, new LoginReply(['Wrong username or password', '', false]));
+    return;
+  }
+  const token = signToken({
+    id: user.id,
+    username: user.username,
+    app: request.app,
+  });
+
+  callback(null, new LoginReply(['', token, true]));
 }
