f9bb020c39778e8a7a64f06b480b947277fc9d25
Trond Bergquist <trond_bergquist@hotmail.com>
2019-08-30 20:04:26 +0200

    graphql: add tvShowDetail query

---
+++ packages/graphql-tvhelper/src/tvshow/dataloaders/TvDetailLoader.js
@@ -0,0 +1,40 @@
+// @flow
+
+import Dataloader from 'dataloader';
+import { fetch, type TvShow } from '@tbergq/graphql-services';
+
+const fetchTvDetail = async (ids: $ReadOnlyArray<string>) => {
+  const responses = await Promise.all(
+    ids.map(id =>
+      fetch(
+        `http://api.tvmaze.com/shows/${id}?embed[]=episodes&embed[]=nextepisode&embed[]=previousepisode&embed[]=cast`,
+      ),
+    ),
+  );
+  return responses.map(response => {
+    const nextDate = response?._embedded?.nextepisode?.airdate ?? null;
+    const previousDate = response?._embedded?.previousepisode?.airdate ?? null;
+    return {
+      ...response,
+      ...(response._embedded
+        ? {
+            _embedded: {
+              ...response._embedded,
+              nextepisode: {
+                ...response._embedded.nextepisode,
+                airdate: nextDate !== null ? new Date(nextDate) : null,
+              },
+              previousepisode: {
+                ...response._embedded.previousepisode,
+                airdate: previousDate !== null ? new Date(previousDate) : null,
+              },
+            },
+          }
+        : {}),
+    };
+  });
+};
+
+const TvDetailLoader = () => new Dataloader<string, TvShow>(fetchTvDetail);
+
+export default TvDetailLoader;
---
+++ packages/graphql-tvhelper/src/tvshow/queries/TvShowDetail.js
@@ -0,0 +1,27 @@
+// @flow
+
+import { GraphQLID, GraphQLNonNull } from 'graphql';
+import { fromGlobalId } from '@kiwicom/graphql-global-id';
+import { type GraphqlContextType } from '@tbergq/graphql-services';
+
+import TvShow from '../types/output/TvShow';
+
+type Args = {|
+  +id: string,
+|};
+
+export default {
+  name: 'tvShowDetail',
+  description: 'Tv show lookup by id',
+  type: TvShow,
+  args: {
+    id: {
+      type: GraphQLNonNull(GraphQLID),
+    },
+  },
+  resolve: async (_: mixed, args: Args, { dataLoader }: GraphqlContextType) => {
+    const id = fromGlobalId(args.id);
+    const tvShow = await dataLoader.tvhelper.tvDetail.load(id);
+    return tvShow;
+  },
+};
