a4df1542ddd4632983862f574b7df424f55d1546
Trond Bergquist <trond_bergquist@hotmail.com>
2019-08-31 12:38:09 +0200

    graphql: Add episode list to tv show

--- packages/graphql-tvhelper/getDataloaders.js
+++ packages/graphql-tvhelper/getDataloaders.js
@@ -5,11 +5,13 @@ import { UserRepository } from '@tbergq/tvhelper-persistence';
 import SearchTvShowLoader from './src/search/dataloaders/SearchTvShowLoader';
 import UserLoader from './src/account/dataloaders/UserLoader';
 import TvDetailLoader from './src/tvshow/dataloaders/TvDetailLoader';
+import EpisodesLoader from './src/episode/dataloaders/EpisodesLoader';
 
 export default function getDataloaders() {
   return {
     searchTvShow: SearchTvShowLoader(),
     user: UserLoader(new UserRepository()),
     tvDetail: TvDetailLoader(),
+    episodes: EpisodesLoader(),
   };
 }
