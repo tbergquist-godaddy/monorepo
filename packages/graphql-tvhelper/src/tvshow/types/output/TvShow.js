df63d4a4d09afb369fbbe2c602af64e2d9f8d6ce
Trond Bergquist <trond_bergquist@hotmail.com>
2019-08-31 11:59:33 +0200

    graphql: Extend tvDetail
    Add image, summary cast fields

--- packages/graphql-tvhelper/src/tvshow/types/output/TvShow.js
+++ packages/graphql-tvhelper/src/tvshow/types/output/TvShow.js
@@ -1,17 +1,18 @@
 // @flow
 
-import { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLFloat } from 'graphql';
-import { toGlobalId } from 'graphql-relay';
+import { GraphQLObjectType, GraphQLString, GraphQLFloat, GraphQLList } from 'graphql';
 import { GraphQLDate } from 'graphql-iso-date';
 import type { TvShow } from '@tbergq/graphql-services';
+import GlobalID from '@kiwicom/graphql-global-id';
+
+import TvHelperImage from '../../../common/types/output/TvHelperImage';
+import Summary from './Summary';
+import Cast from '../../../common/types/output/Cast';
 
 export default new GraphQLObjectType({
   name: 'TvShow',
   fields: {
-    id: {
-      type: GraphQLID,
-      resolve: ({ id }: TvShow) => toGlobalId('tvShow', id.toString()),
-    },
+    id: GlobalID(({ id }) => id),
     name: {
       type: GraphQLString,
     },
@@ -21,9 +22,17 @@ export default new GraphQLObjectType({
     premiered: {
       type: GraphQLDate,
     },
+    image: {
+      type: TvHelperImage,
+    },
     rating: {
       type: GraphQLFloat,
       resolve: ({ rating }: TvShow) => rating.average,
+    },
+    summary: Summary,
+    cast: {
+      type: GraphQLList(Cast),
+      resolve: ({ _embedded }: TvShow) => _embedded?.cast,
     },
   },
 });
