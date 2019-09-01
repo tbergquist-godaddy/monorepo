37454a8870b30ad0fe256e4784bb193921fb8f9b
Trond Bergquist <trond_bergquist@hotmail.com>
2019-09-01 11:45:24 +0200

    graphql: Add is favorite field

---
+++ packages/graphql-tvhelper/src/tvshow/types/output/__tests__/TvShow.test.js
@@ -0,0 +1,46 @@
+// @flow
+
+import { toGlobalId } from 'graphql-relay';
+
+import TvShow from '../TvShow';
+
+const fields = TvShow.getFields();
+
+const { resolve } = fields.isFavorite;
+
+const ancestor = { id: 1 };
+const args = null;
+
+it('returns null when not logged in', async () => {
+  expect(await resolve(ancestor, args, { user: null, dataLoader: {} })).toBeNull();
+});
+
+it('returns true when show is favorite', async () => {
+  const user = {
+    id: toGlobalId('user', '1'),
+  };
+  const dataLoader = {
+    tvhelper: {
+      favorites: {
+        load: () => Promise.resolve([{ userId: 1, serieId: 1 }]),
+      },
+    },
+  };
+  const context = { user, dataLoader };
+  expect(await resolve(ancestor, args, context)).toBe(true);
+});
+
+it('returns false when show is not favorite', async () => {
+  const user = {
+    id: toGlobalId('user', '1'),
+  };
+  const dataLoader = {
+    tvhelper: {
+      favorites: {
+        load: () => Promise.resolve([]),
+      },
+    },
+  };
+  const context = { user, dataLoader };
+  expect(await resolve(ancestor, args, context)).toBe(false);
+});
