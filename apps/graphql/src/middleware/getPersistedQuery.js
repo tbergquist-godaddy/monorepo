8dbcd22c3ef4fc25db17d8ce46c835e59ad342d8
Trond Bergquist <trond_bergquist@hotmail.com>
2019-09-07 15:57:39 +0200

    graphql: use persisted queries

---
+++ apps/graphql/src/middleware/getPersistedQuery.js
@@ -0,0 +1,26 @@
+// @flow
+
+import bodyParser from 'body-parser';
+import type { $Request, $Response } from 'express';
+import { StoredOperationRepository } from '@tbergq/graphql-persistence';
+
+const jsonParser = bodyParser.json();
+
+export default function matchQueryMiddleware() {
+  return (req: $Request, res: $Response, next: Function) => {
+    return jsonParser(req, res, async () => {
+      const { queryId } = req.body;
+      if (typeof queryId === 'string') {
+        const query = await StoredOperationRepository.getOperationText(queryId);
+
+        if (query) {
+          // eslint-disable-next-line require-atomic-updates
+          req.body.query = query;
+        } else {
+          throw new Error(`matchQueryMiddleware: can't find queryId: ${queryId}`);
+        }
+      }
+      next();
+    });
+  };
+}
