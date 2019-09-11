8b17af4408db0bd5422d2d653c4e26bc2a5be011
Trond Bergquist <trond_bergquist@hotmail.com>
2019-09-11 19:14:12 +0200

    tvhelper: ssr post search request
    get initial props is unable to read body.
    Therefor we need to call api endpoint, and redirect
    to get method. This is for anyone with javascript turned off

--- apps/trainingjournal/src/programDetail/__tests__/Days.test.js
+++ apps/trainingjournal/src/programDetail/__tests__/Days.test.js
@@ -9,32 +9,34 @@ import Days from '../Days';
 
 const renderInner = props => <Days week={props.program.weeks.edges[0].node} />;
 
-test('Days', () => {
-  const environment = createMockEnvironment();
-  const TestRenderer = () => (
-    <QueryRenderer
-      environment={environment}
-      query={graphql`
-        query DaysQuery @relay_test_operation {
-          program(programId: "123") {
-            weeks {
-              edges {
-                node {
-                  ...Days_week
+describe('Days', () => {
+  it('renders', () => {
+    const environment = createMockEnvironment();
+    const TestRenderer = () => (
+      <QueryRenderer
+        environment={environment}
+        query={graphql`
+          query DaysQuery @relay_test_operation {
+            program(programId: "123") {
+              weeks {
+                edges {
+                  node {
+                    ...Days_week
+                  }
                 }
               }
             }
           }
-        }
-      `}
-      variables={{}}
-      onResponse={renderInner}
-    />
-  );
-  const renderer = create(<TestRenderer />);
-  environment.mock.resolveMostRecentOperation(operation =>
-    MockPayloadGenerator.generate(operation),
-  );
+        `}
+        variables={{}}
+        onResponse={renderInner}
+      />
+    );
+    const renderer = create(<TestRenderer />);
+    environment.mock.resolveMostRecentOperation(operation =>
+      MockPayloadGenerator.generate(operation),
+    );
 
-  expect(renderer).toMatchSnapshot();
+    expect(renderer).toMatchSnapshot();
+  });
 });
