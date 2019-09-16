a00bc7e71f326136f41dcfe304d44f32ff4f4a4a
Trond Bergquist <trond_bergquist@hotmail.com>
2019-09-16 17:14:57 +0200

    Upgrade dependencies

--- apps/trainingjournal/src/day/__tests__/DayDetail.test.js
+++ apps/trainingjournal/src/day/__tests__/DayDetail.test.js
@@ -2,17 +2,15 @@
 
 import * as React from 'react';
 import { create, act } from 'react-test-renderer';
-import { createMockEnvironment, MockPayloadGenerator } from 'relay-test-utils';
-import { QueryRenderer, graphql } from '@kiwicom/relay';
+import { MockPayloadGenerator } from 'relay-test-utils';
+import { QueryRenderer, graphql, Environment } from '@tbergq/relay';
 
 import DayDetail from '../DayDetail';
 import AddEcerciseForm from '../AddExerciseForm';
 
 const renderer = props => <DayDetail day={props.day} />;
-let environment;
 const TestRenderer = () => (
   <QueryRenderer
-    environment={environment}
     query={graphql`
       query DayDetailQuery @relay_test_operation {
         day(dayId: "123") {
@@ -20,48 +18,49 @@ const TestRenderer = () => (
         }
       }
     `}
-    onResponse={renderer}
+    render={renderer}
+    variables={{}}
   />
 );
 
-beforeEach(() => {
-  environment = createMockEnvironment();
-});
-
-it('renders', () => {
-  const wrapper = create(<TestRenderer />);
-  environment.mock.resolveMostRecentOperation(operation =>
-    MockPayloadGenerator.generate(operation, {
-      Exercise: () => ({
-        set: '4',
-        reps: '5',
-        breakTime: '2',
+describe('DayDetail', () => {
+  it('renders', () => {
+    const environment = Environment.getEnvironment();
+    const wrapper = create(<TestRenderer />);
+    environment.mock.resolveMostRecentOperation(operation =>
+      MockPayloadGenerator.generate(operation, {
+        Exercise: () => ({
+          set: '4',
+          reps: '5',
+          breakTime: '2',
+        }),
+        BaseExercise: () => ({
+          name: 'Knebøy',
+        }),
       }),
-      BaseExercise: () => ({
-        name: 'Knebøy',
-      }),
-    }),
-  );
-  expect(wrapper).toMatchSnapshot();
-});
-
-it('shows form when add button is clicked', () => {
-  const wrapper = create(<TestRenderer />);
-  environment.mock.resolveMostRecentOperation(operation =>
-    MockPayloadGenerator.generate(operation, {
-      Exercise: () => ({
-        set: '4',
-        reps: '5',
-        breakTime: '2',
-      }),
-      BaseExercise: () => ({
-        name: 'Knebøy',
-      }),
-    }),
-  );
-  const fab = wrapper.root.findByProps({ 'data-test': 'fab' });
-  act(() => {
-    fab.props.onClick();
+    );
+    expect(wrapper).toMatchSnapshot();
   });
-  expect(wrapper.root.findByType(AddEcerciseForm)).not.toBeNull();
+
+  it('shows form when add button is clicked', () => {
+    const environment = Environment.getEnvironment();
+    const wrapper = create(<TestRenderer />);
+    environment.mock.resolveMostRecentOperation(operation =>
+      MockPayloadGenerator.generate(operation, {
+        Exercise: () => ({
+          set: '4',
+          reps: '5',
+          breakTime: '2',
+        }),
+        BaseExercise: () => ({
+          name: 'Knebøy',
+        }),
+      }),
+    );
+    const fab = wrapper.root.findByProps({ 'data-test': 'fab' });
+    act(() => {
+      fab.props.onClick();
+    });
+    expect(wrapper.root.findByType(AddEcerciseForm)).not.toBeNull();
+  });
 });
