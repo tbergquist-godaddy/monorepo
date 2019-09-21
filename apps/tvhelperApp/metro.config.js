9d90ca6a0c44a9d26641f32423de9fdbe60ec0e2
Trond Bergquist <trond_bergquist@hotmail.com>
2019-09-21 16:57:33 +0200

    Add tvhelper app

---
+++ apps/tvhelperApp/metro.config.js
@@ -0,0 +1,28 @@
+// @flow
+
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const path = require('path');
+
+const cwd = path.resolve(__dirname);
+
+module.exports = {
+  watchFolders: [
+    path.resolve(cwd, '..', '..', 'packages'),
+    path.resolve(cwd, '..', '..', 'node_modules'),
+  ],
+  transformer: {
+    // eslint-disable-next-line require-await
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
