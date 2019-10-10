be2107592ac9e0733c9458b8e53b3ce44d1d727f
Trond Bergquist <trond_bergquist@hotmail.com>
2019-10-10 09:54:43 +0200

    tvh: SSR tvShow page

--- apps/tvhelper/pages/tvShow.js
+++ apps/tvhelper/pages/tvShow.js
@@ -3,19 +3,20 @@
 import * as React from 'react';
 import { useRouter } from 'next/router';
 import { getNextToken } from '@tbergq/utils';
-import { QueryRendererProvider } from '@tbergq/relay';
+import { QueryRendererProvider, fetchQuery, Environment } from '@tbergq/relay';
 
 import Layout from '../src/components/Layout';
-import TvShowQuery from '../src/tvShow/TvShowQuery';
+import TvShowQuery, { tvShowQuery } from '../src/tvShow/TvShowQuery';
 
 type Props = {|
+  +json: {| +[key: string]: mixed |},
   +token: ?string,
 |};
 
 function TvShowPage(props: Props) {
   const router = useRouter();
   return (
-    <QueryRendererProvider initialValue={null} token={props.token}>
+    <QueryRendererProvider initialValue={props.json} token={props.token}>
       <Layout isLoggedIn={props.token != null}>
         <TvShowQuery tvShowId={router.query.id} />
       </Layout>
@@ -23,8 +24,19 @@ function TvShowPage(props: Props) {
   );
 }
 
-TvShowPage.getInitialProps = ctx => {
-  return { token: getNextToken(ctx) };
+TvShowPage.getInitialProps = async ctx => {
+  const token = getNextToken(ctx);
+
+  const environment = Environment.getEnvironment(token);
+  const id = ctx.query?.id;
+  await fetchQuery(environment, tvShowQuery, { id });
+
+  const json = environment
+    .getStore()
+    .getSource()
+    .toJSON();
+
+  return { json, token };
 };
 
 export default TvShowPage;
