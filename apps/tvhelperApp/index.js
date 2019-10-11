5da5769cb30c173a538ac5be29c39efa1fc2ccb7
Trond Bergquist <trond_bergquist@hotmail.com>
2019-10-11 19:28:28 +0200

    Upgrade rn to 0.61.2

--- apps/tvhelperApp/index.js
+++ apps/tvhelperApp/index.js
@@ -5,7 +5,6 @@
  */
 
 import { AppRegistry, YellowBox } from 'react-native';
-import 'react-native-gesture-handler';
 
 import App from './src/navigation';
 import { name as appName } from './app.json';
