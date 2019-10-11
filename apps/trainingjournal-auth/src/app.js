a30e8900334ae43b13936b2188a1b1dc9e02f8b7
Trond Bergquist <trond_bergquist@hotmail.com>
2019-10-11 21:21:26 +0200

    tja: Add cors, compression, morgan

--- apps/trainingjournal-auth/src/app.js
+++ apps/trainingjournal-auth/src/app.js
@@ -1,10 +1,17 @@
 // @flow strict
 
 import express from 'express';
+import cors from 'cors';
+import compression from 'compression';
+import morgan from 'morgan';
 
 import registerRoutes from './api/router';
 
 const app = express();
+
+app.use(cors());
+app.use(compression());
+app.use(morgan('dev'));
 
 registerRoutes(app);
 
