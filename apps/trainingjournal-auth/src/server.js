2a705be5785c7ed231f3586881138eebdd64b5c1
Trond Bergquist <trond_bergquist@hotmail.com>
2019-10-12 15:27:37 +0200

    tja: Add swagger

--- apps/trainingjournal-auth/src/server.js
+++ apps/trainingjournal-auth/src/server.js
@@ -2,13 +2,16 @@
 
 import { config } from 'dotenv';
 import { invariant } from '@kiwicom/js';
+import swaggerUI from 'swagger-ui-express';
 
+import swaggerDocument from './swagger.json';
 import app from './app';
 import connection from './db/connection';
 
 config();
 const { PORT, DB_URL } = process.env;
 
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 invariant(DB_URL != null, 'Expected to have DB_URL environment varible, but it was undefined');
 
 connection.openUri(DB_URL, {
