a6ced48b616e6dc347a02086180e962a80c75368
Trond Bergquist <trond_bergquist@hotmail.com>
2019-10-12 13:27:09 +0200

    tja: encrypt password

--- apps/trainingjournal-auth/src/db/repositories/UserRepository.js
+++ apps/trainingjournal-auth/src/db/repositories/UserRepository.js
@@ -1,5 +1,7 @@
 // @flow strict
 
+import crypto from 'crypto';
+
 import Model from '../models/UserModel';
 import User from '../dataObjects/User';
 
@@ -9,14 +11,29 @@ type CreateUserInput = {|
   +email: string,
 |};
 
+const genRandomString = (length: number) => {
+  return crypto
+    .randomBytes(Math.ceil(length / 2))
+    .toString('hex')
+    .slice(0, length);
+};
+
+const encryptPassword = (password: string, salt: string) => {
+  const hash = crypto.createHmac('sha512', salt);
+  hash.update(password);
+  return hash.digest('hex');
+};
+
 export default class UserRepository {
   static async createUser(input: CreateUserInput) {
-    // TODO: Encrypt password
-    const salt = 'salt';
+    const salt = genRandomString(16);
+    const password = encryptPassword(input.password, salt);
     const user = await Model.create({
       ...input,
+      password,
       salt,
     });
+
     return new User(user);
   }
 }
---
+++ apps/trainingjournal-auth/src/db/repositories/__tests__/UserRepository.test.js
@@ -0,0 +1,34 @@
+// @flow strict
+
+import Model from '../../models/UserModel';
+import UserRepository from '../UserRepository';
+import connection from '../../connection';
+
+describe('UserRepository', () => {
+  afterEach(async () => {
+    await connection.collection('users').drop();
+  });
+  it('hashes password', async () => {
+    await UserRepository.createUser({
+      username: 'løken',
+      password: 'I_am_naive',
+      email: 'lol@lol.no',
+    });
+
+    const user = await Model.findOne({ username: 'løken' });
+    expect(user.password).toBeDefined();
+    expect(user.password).not.toEqual('I_am_naive');
+  });
+
+  it('does not return the password', async () => {
+    const user = await UserRepository.createUser({
+      username: 'løken',
+      password: 'I_am_naive',
+      email: 'lol@lol.no',
+    });
+
+    expect(user.username).toEqual('løken');
+    expect(user.email).toEqual('lol@lol.no');
+    expect(user.password).toBeNull();
+  });
+});
