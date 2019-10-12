b92ea337d27b8a8791f6ace6b8c6d2dd58360f71
Trond Bergquist <trond_bergquist@hotmail.com>
2019-10-12 13:27:09 +0200

    tja: encrypt password

--- apps/trainingjournal-auth/src/db/dataObjects/User.js
+++ apps/trainingjournal-auth/src/db/dataObjects/User.js
@@ -5,13 +5,14 @@ import type { UserType } from '../models/UserModel';
 export default class User {
   id: string;
   username: string;
-  password: string;
+  password: string | null;
   email: string;
 
-  constructor(user: UserType) {
+  constructor(user: UserType, includePassword: boolean = false) {
     this.id = user._id.toString();
     this.username = user.username;
-    this.password = user.password;
+    this.password = includePassword ? user.password : null;
+
     this.email = user.email;
   }
 }
