57f8a9c70adc2fc6697954eacd04e0646dbeffcd
Trond Bergquist <trond_bergquist@hotmail.com>
2019-10-13 14:56:38 +0200

    Create nextjs-utils package
    move nProgress as a HOC to nextjs-utils package

--- apps/tvhelper/pages/_app.js
+++ apps/tvhelper/pages/_app.js
@@ -2,33 +2,14 @@
 
 import * as React from 'react';
 import App from 'next/app';
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
+import { withNProgress } from '@tbergq/nextjs-utils';
 
-export default class MyApp extends App {
-  componentDidMount = () => {
-    NProgress.configure({ showSpinner: false });
-    Router.events.on('routeChangeStart', this.handleRouteChangeStart);
-    Router.events.on('routeChangeComplete', this.handleRouteChangeComplete);
-    Router.events.on('routeChangeError', this.handleRouteChangeComplete);
-  };
-
-  componentWillUnmount = () => {
-    Router.events.off('routeChangeStart', this.handleRouteChangeStart);
-  };
-
-  handleRouteChangeStart = () => {
-    NProgress.start();
-  };
-
-  handleRouteChangeComplete = () => {
-    NProgress.done();
-  };
-
+class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
     return <Component {...pageProps} />;
   }
 }
+
+export default withNProgress(MyApp);
