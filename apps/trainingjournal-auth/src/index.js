6742fd419d7cc32bdd1228d1e983e40f05e89fd4
Trond Bergquist <trond_bergquist@hotmail.com>
2019-10-13 11:25:53 +0200

    tja: Fix swagger deployment

--- apps/trainingjournal-auth/src/index.js
+++ apps/trainingjournal-auth/src/index.js
@@ -4,7 +4,7 @@ import { config } from 'dotenv';
 import { invariant } from '@kiwicom/js';
 import swaggerUI from 'swagger-ui-express';
 
-import swaggerDocument from './swagger.json';
+import swaggerDocument from './swagger';
 import app from './app';
 import connection from './db/connection';
 
