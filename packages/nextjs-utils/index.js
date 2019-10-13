cba47ed4a68f47e0518e241cecbc7dfbfa578b47
Trond Bergquist <trond_bergquist@hotmail.com>
2019-10-13 15:05:13 +0200

    next-utils: Add withDocument HOC

--- packages/nextjs-utils/index.js
+++ packages/nextjs-utils/index.js
@@ -1,3 +1,4 @@
 // @flow strict
 
 export { default as withNProgress } from './src/withNProgress';
+export { default as withDocument } from './src/withDocument';
---
+++ packages/nextjs-utils/src/withDocument.js
@@ -0,0 +1,75 @@
+// @flow
+
+import * as React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet, createGlobalStyle } from 'styled-components';
+import { AppRegistry } from 'react-native';
+
+const GlobalStyle = createGlobalStyle({
+  html: {
+    height: '100%',
+  },
+  body: {
+    fontFamily: 'Roboto, sans-serif',
+  },
+  '#nprogress .bar': {
+    height: '4px',
+  },
+  '#__next': {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+  },
+});
+
+export default function withDocument(appName: string) {
+  return class MyDocument extends Document {
+    static async getInitialProps(ctx: any) {
+      const sheet = new ServerStyleSheet();
+      const originalRenderPage = ctx.renderPage;
+      AppRegistry.registerComponent(appName, () => Main);
+      // $FlowExpectedError: This is react-native-web specific
+      const { getStyleElement } = AppRegistry.getApplication(appName);
+      try {
+        ctx.renderPage = () =>
+          originalRenderPage({
+            enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+          });
+
+        const initialProps = await Document.getInitialProps(ctx);
+        return {
+          ...initialProps,
+          styles: (
+            <>
+              {initialProps.styles}
+              {sheet.getStyleElement()}
+              {getStyleElement()}
+              <GlobalStyle />
+            </>
+          ),
+        };
+      } finally {
+        sheet.seal();
+      }
+    }
+
+    render() {
+      return (
+        <html lang="en">
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link
+              href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700"
+              rel="stylesheet"
+            />
+            <GlobalStyle />
+          </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </html>
+      );
+    }
+  };
+}
