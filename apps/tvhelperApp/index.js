ae115698abe0e341b7d02d66b6ea0857f36801b7
Trond Bergquist <trond_bergquist@hotmail.com>
2019-10-15 08:17:22 +0200

    Add rn-gesture-handler import
    This is needed for production builds

--- apps/tvhelperApp/index.js
+++ apps/tvhelperApp/index.js
@@ -5,6 +5,7 @@
  */
 
 import { AppRegistry, YellowBox } from 'react-native';
+import 'react-native-gesture-handler';
 
 import App from './src/navigation';
 import { name as appName } from './app.json';
