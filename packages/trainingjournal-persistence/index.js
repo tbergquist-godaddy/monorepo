d76dfd75b3b98204a90140b0780a7031e06eff5f
Trond Bergquist <trond_bergquist@hotmail.com>
2019-10-26 17:48:59 +0200

    Move tj-auth under graphql

---
+++ packages/trainingjournal-persistence/index.js
@@ -0,0 +1,7 @@
+// @flow strict
+
+// Repositories
+export { default as UserRepository } from './src/repositories/UserRepository';
+
+// Connection
+export { default as trainingJournalConnection } from './src/connection';
--- scripts/mongoMemoryServer.js
+++ scripts/mongoMemoryServer.js
@@ -3,9 +3,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import { tvHelperConnection } from '@tbergq/tvhelper-persistence';
 import { graphqlConnection } from '@tbergq/graphql-persistence';
+import { trainingJournalConnection } from '@tbergq/trainingjournal-persistence';
 
 let tvhelperServer;
 let graphqlMongoServer;
+let tjMongoServer;
 const opts = { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true };
 
 beforeAll(async () => {
@@ -15,12 +17,16 @@ beforeAll(async () => {
   graphqlMongoServer = new MongoMemoryServer();
   const graphqlMongoServerUri = await graphqlMongoServer.getConnectionString();
 
+  tjMongoServer = new MongoMemoryServer();
+  const tjMongoServerUri = await graphqlMongoServer.getConnectionString();
+
   await Promise.all([
     tvHelperConnection.openUri(tvhelperUri, opts),
     graphqlConnection.openUri(graphqlMongoServerUri, opts),
+    trainingJournalConnection.openUri(tjMongoServerUri, opts),
   ]);
 }, 600000);
 
 afterAll(async () => {
-  await Promise.all([tvhelperServer.stop(), graphqlMongoServer.stop()]);
+  await Promise.all([tvhelperServer.stop(), graphqlMongoServer.stop(), tjMongoServer.stop()]);
 }, 5000);
