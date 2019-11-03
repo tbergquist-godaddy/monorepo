9f2c96806a047bbb82b38553a294cad47b3de03c
Trond Bergquist <trond_bergquist@hotmail.com>
2019-11-03 15:25:44 +0100

    Upgrade dependencies

--- packages/components/src/Button.js
+++ packages/components/src/Button.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import OriginalButton, { type Props } from '@kiwicom/orbit-components/lib/Button';
 
 export default function Button(props: Props): React.Element<React.ComponentType<Props>> {
+  // $FlowFixMe (>=<0.111.1)
   return <OriginalButton {...props} />;
 }
 
