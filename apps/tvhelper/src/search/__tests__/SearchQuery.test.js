3a5d4f863686b988ef72ab03da666dc482dd878d
Trond Bergquist <trond_bergquist@hotmail.com>
2019-11-16 17:29:08 +0100

    Delete rn-components

--- apps/tvhelper/src/search/__tests__/SearchQuery.test.js
+++ apps/tvhelper/src/search/__tests__/SearchQuery.test.js
@@ -6,16 +6,17 @@ import { render, configure } from '@testing-library/react';
 import SearchQuery from '../SearchQuery';
 
 describe('SearchQuery', () => {
-  configure({ testIdAttribute: 'data-testid' });
+  configure({ testIdAttribute: 'data-test' });
 
   it('renders queryRenderer loader when query is defined', () => {
     const { getByTestId } = render(<SearchQuery query="test" />);
     expect(getByTestId('queryRenderLoader')).not.toBeNull();
   });
 
-  it('does not render queryRenderer loader when query is empty string', async () => {
-    const { queryByTestId } = render(<SearchQuery query="" />);
-    const qr = await queryByTestId('queryRenderLoader');
-    expect(qr).toBeNull();
+  it('does not render queryRenderer loader when query is empty string', () => {
+    const { getByTestId } = render(<SearchQuery query="" />);
+    expect(() => {
+      getByTestId('queryRenderLoader');
+    }).toThrow();
   });
 });
