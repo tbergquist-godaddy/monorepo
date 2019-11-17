6093bf102f95f67b22044c5143c3f32d7b3dfbc9
Trond Bergquist <trond_bergquist@hotmail.com>
2019-11-17 20:18:58 +0100

    Refactor Toast to hook

--- packages/components/src/__tests__/Toast.test.js
+++ packages/components/src/__tests__/Toast.test.js
@@ -1,51 +1,30 @@
 // @flow
 
 import * as React from 'react';
-import { create } from 'react-test-renderer';
+import { render, act, fireEvent } from '@testing-library/react';
 
 import Toast from '../Toast';
 
 jest.useFakeTimers();
 
-it('renders', () => {
-  expect(create(<Toast onHide={jest.fn()} />)).toMatchInlineSnapshot(`
-    .c0 {
-      position: fixed;
-      bottom: 0;
-      right: 0;
-      min-width: 200px;
-      max-height: 0;
-      z-index: 100;
-      background-color: #0B0C0F;
-      opacity: 0.4;
-      color: #FFFFFF;
-      padding: 0;
-      border-radius: 3px;
-      -webkit-transition: max-height 0.3s ease-in-out;
-      transition: max-height 0.3s ease-in-out;
-    }
-
-    .c1 {
-      padding: 0;
-    }
-
-    <div
-      className="c0"
-    >
-      <div
-        className="c1"
-      />
-    </div>
-  `);
-});
-
+const Wrapper = ({ onHide }) => {
+  const ref = React.useRef(null);
+  return (
+    <>
+      <Toast ref={ref} onHide={onHide} />
+      <button type="button" onClick={() => ref.current?.show()} />
+    </>
+  );
+};
 it('calls onHide', () => {
   const onHide = jest.fn();
-  const wrapper = create(<Toast onHide={onHide} />);
-  const instance = wrapper.getInstance();
-  instance.show('Test');
+  const { container } = render(<Wrapper onHide={onHide} />);
+  const button = container.querySelector('button');
 
-  jest.runAllTimers();
+  act(() => {
+    fireEvent.click(button);
+    jest.runAllTimers();
+  });
 
   expect(onHide).toHaveBeenCalledWith();
 });
