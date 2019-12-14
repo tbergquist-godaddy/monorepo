05c5a1bdac8945c5ba2de02e9cb78690a6607414
Trond Bergquist <trond_bergquist@hotmail.com>
2019-12-14 16:35:57 +0100

    tvh: Use js-esm as build target

--- apps/tvhelper/.babelrc.js
+++ apps/tvhelper/.babelrc.js
@@ -1,11 +1,24 @@
 // @flow
 
-module.exports = {
-  presets: ['@adeira/babel-preset-adeira', 'next/babel'],
-  plugins: [
-    ['styled-components', { ssr: true }],
-    'relay',
-    '@babel/plugin-proposal-class-properties',
-    '@kiwicom/babel-plugin-orbit-components'
-  ],
+const plugins = [
+  ['styled-components', { ssr: true }],
+  'relay',
+  '@babel/plugin-proposal-class-properties',
+  '@kiwicom/orbit-components'
+];
+
+const prodAndDev = {
+  presets:  ['next/babel', ['@adeira/babel-preset-adeira', { target: 'js-esm' }]],
+  plugins,
 };
+module.exports = {
+  env: {
+    production: prodAndDev,
+    development: prodAndDev,
+    test: {
+      presets: ['@adeira/babel-preset-adeira', 'next/babel'],
+      plugins,
+    },
+  },
+};
+
