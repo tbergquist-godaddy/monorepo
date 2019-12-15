dc7237c1d1eb47f7eb00296761bbf9fce65a2cac
Trond Bergquist <trond_bergquist@hotmail.com>
2019-12-15 17:11:51 +0100

    gql: Add delete exercise mutation

---
+++ apps/graphql/src/trainingjournal/exercise/mutations/DeleteExercise.js
@@ -0,0 +1,37 @@
+// @flow
+
+import { GraphQLNonNull, GraphQLID } from 'graphql';
+import { ExerciseRepository } from '@tbergq/trainingjournal-persistence';
+import { fromGlobalId } from '@adeira/graphql-global-id';
+
+import type { GraphqlContextType } from '../../../services/createGraphqlContext';
+import DeletedExercise from '../types/output/DeletedExercise';
+
+type Args = {
+  +id: string,
+  ...
+};
+
+export default {
+  name: 'DeleteExercise',
+  description: 'Delete an Exercise',
+  type: DeletedExercise,
+  args: {
+    id: {
+      type: GraphQLNonNull(GraphQLID),
+    },
+  },
+  resolve: async (_: mixed, { id }: Args, { user }: GraphqlContextType) => {
+    const userId = user?.id;
+    if (userId == null) {
+      return null;
+    }
+    const exerciseId = fromGlobalId(id);
+    const isDeleted = await ExerciseRepository.deleteExercise(userId, exerciseId);
+
+    return {
+      success: isDeleted,
+      exerciseId: isDeleted ? id : null,
+    };
+  },
+};
