117204145c9a67d5f0ca8e0b895e3b4cd0ba41d1
Trond Bergquist <trond_bergquist@hotmail.com>
2020-01-04 14:11:22 +0100

    gql: Add programs connection to tjviewer

--- apps/graphql/src/trainingjournal/types/output/TraningJournalViewer.js
+++ apps/graphql/src/trainingjournal/types/output/TraningJournalViewer.js
@@ -6,7 +6,18 @@ import { connectionArgs, type ConnectionArguments, cursorToOffset } from 'graphq
 
 import type { GraphqlContextType } from '../../../services/createGraphqlContext';
 import ExerciseConnection from '../../exercise/types/output/ExerciseConnection';
+import ProgramsConnection from '../../programs/types/output/ProgramConnection';
 import toConnection from '../../../services/toConnection';
+
+const getUserAndOffset = (
+  args: ConnectionArguments,
+  user: $PropertyType<GraphqlContextType, 'user'>,
+) => {
+  return {
+    userId: user?.id,
+    offset: args.after != null ? cursorToOffset(args.after) + 1 : 0,
+  };
+};
 
 export default new GraphQLObjectType({
   name: 'TraningJournalViewer',
@@ -28,11 +39,12 @@ export default new GraphQLObjectType({
         args: ConnectionArguments,
         { user, dataLoader }: GraphqlContextType,
       ) => {
-        const userId = user?.id;
+        const { userId, offset } = getUserAndOffset(args, user);
+
         if (userId == null) {
           return null;
         }
-        const offset = args.after != null ? cursorToOffset(args.after) + 1 : 0;
+
         const { exercises, count } = await dataLoader.trainingjournal.exercises.load({
           userId,
           limit: args.first ?? 20,
@@ -43,6 +55,27 @@ export default new GraphQLObjectType({
           offset,
           count,
         });
+      },
+    },
+    programs: {
+      type: ProgramsConnection,
+      args: connectionArgs,
+      resolve: async (
+        _: mixed,
+        args: ConnectionArguments,
+        { user, dataLoader }: GraphqlContextType,
+      ) => {
+        const { userId, offset } = getUserAndOffset(args, user);
+
+        if (userId == null) {
+          return null;
+        }
+        const { programs, count } = await dataLoader.trainingjournal.programs.load({
+          userId,
+          limit: args.first ?? 20,
+          skip: offset,
+        });
+        return toConnection(programs, { offset, count });
       },
     },
   },
