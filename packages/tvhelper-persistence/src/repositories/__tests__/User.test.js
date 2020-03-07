658b8a2d37128e97e880e4beec17f1784eb944bc
Trond Bergquist <trond_bergquist@hotmail.com>
2020-03-07 12:46:21 +0100

    tvhp: Add change password method

--- packages/tvhelper-persistence/src/repositories/__tests__/User.test.js
+++ packages/tvhelper-persistence/src/repositories/__tests__/User.test.js
@@ -1,23 +1,43 @@
 // @flow
 
-import mockingoose from 'mockingoose';
-
-import model from '../../models/UserModel';
 import UserRepository from '../User';
+import connection from '../../connection';
+
+const user1 = {
+  username: 'lol',
+  password: 'lol',
+  email: 'lol@lol.no',
+};
+let user1Id;
+
+beforeEach(async () => {
+  const { id: userId1 } = await UserRepository.createUser(user1);
+  user1Id = userId1;
+});
+
+afterEach(async () => {
+  await connection.collection('users').drop();
+});
 
 it('should find the user with findOne', async () => {
-  const user = {
-    _id: '507f191e810c19729de860ea',
-    username: 'name',
-    email: 'name@email.com',
-    password: 'password',
-  };
+  const dbuser = await UserRepository.findUser(user1.username);
 
-  mockingoose(model).toReturn(user, 'findOne');
-  const dbuser = await UserRepository.findUser('name');
+  expect(dbuser?.id).toEqual(user1Id);
+  expect(dbuser?.username).toEqual(user1.username);
+  expect(dbuser?.email).toEqual(user1.email);
+});
 
-  expect(dbuser?.id).toEqual(user._id);
-  expect(dbuser?.username).toEqual(user.username);
-  expect(dbuser?.email).toEqual(user.email);
-  expect(dbuser?.password).toEqual(user.password);
+it('should change password', async () => {
+  const dbuser = await UserRepository.findUser(user1.username);
+  const hashedPassword = dbuser?.password;
+
+  await UserRepository.changePassword(user1.username, user1.password, 'omg');
+  const withNewPassword = await UserRepository.findUser(user1.username);
+  expect(withNewPassword?.password).not.toEqual(hashedPassword);
+});
+
+it('should throw an error if you have wrong password', async () => {
+  await expect(
+    UserRepository.changePassword(user1.username, `${user1.password} wrong`, 'omg'),
+  ).rejects.toThrow('Wrong password');
 });
