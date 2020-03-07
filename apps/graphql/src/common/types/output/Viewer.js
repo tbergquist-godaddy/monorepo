9358f2883442d82a1a7bee8154135c6eee939be6
Trond Bergquist <trond_bergquist@hotmail.com>
2020-03-07 14:26:40 +0100

    graphql: Add tvhelper viewer type

--- apps/graphql/src/common/types/output/Viewer.js
+++ apps/graphql/src/common/types/output/Viewer.js
@@ -3,14 +3,17 @@
 import { GraphQLUnionType } from 'graphql';
 
 import TJViewer from '../../../trainingjournal/types/output/TraningJournalViewer';
+import TVHViewer from '../../../tvhelper/account/types/output/TvHelperViewer';
 import Unauthorized from './Unauthorized';
 
 export default new GraphQLUnionType({
   name: 'Viewer',
-  types: [TJViewer, Unauthorized],
+  types: [TJViewer, TVHViewer, Unauthorized],
   resolveType: value => {
     if (value === 'trainingjournal') {
       return TJViewer;
+    } else if (value === 'tvhelper') {
+      return TVHViewer;
     }
     return Unauthorized;
   },
