2cad23f85bcfd3348b25ad32f4c4f955be05cac0
Trond Bergquist <trond_bergquist@hotmail.com>
2020-03-14 10:32:00 +0100

    Fix button flow type

--- packages/components/src/Button.js
+++ packages/components/src/Button.js
@@ -4,8 +4,7 @@ import * as React from 'react';
 import { Button as OriginalButton } from '@kiwicom/orbit-components';
 import { type Props } from '@kiwicom/orbit-components/lib/Button';
 
-export default function Button(props: Props): React.Element<React.ComponentType<Props>> {
-  // $FlowFixMe (>=<0.111.1)
+export default function Button(props: Props): React.Element<typeof OriginalButton> {
   return <OriginalButton {...props} />;
 }
 
