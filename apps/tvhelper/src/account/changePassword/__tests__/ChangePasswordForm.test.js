88ad11b6e431d240800f8751950a7bf8a6cba721
Trond Bergquist <trond_bergquist@hotmail.com>
2020-03-14 18:31:53 +0100

    tvh: Add change password form

---
+++ apps/tvhelper/src/account/changePassword/__tests__/ChangePasswordForm.test.js
@@ -0,0 +1,63 @@
+// @flow
+
+import * as React from 'react';
+import { RelayEnvironmentProvider } from '@tbergq/relay';
+import { createMockEnvironment } from 'relay-test-utils';
+import { render, act, fireEvent } from '@testing-library/react';
+
+import ChangePasswordForm from '../ChangePasswordForm';
+
+let environment;
+
+beforeEach(() => {
+  environment = createMockEnvironment();
+});
+
+const TestRenderer = () => (
+  <RelayEnvironmentProvider environment={environment}>
+    <ChangePasswordForm />
+  </RelayEnvironmentProvider>
+);
+
+const getInputs = container => {
+  const password = container.querySelector('input[name="password"]');
+  const newPassword = container.querySelector('input[name="newPassword"]');
+  const confirmPassword = container.querySelector('input[name="confirmPassword"]');
+
+  return {
+    password,
+    newPassword,
+    confirmPassword,
+  };
+};
+
+it('show required warnings', async () => {
+  const { container, getAllByText } = render(<TestRenderer />);
+
+  const button = container.querySelector('button[type="submit"]');
+  await act(async () => {
+    await fireEvent.click(button);
+  });
+
+  const errors = getAllByText('This field is required');
+  expect(errors).toHaveLength(3);
+});
+
+it('show error if confirm password does not match new password', async () => {
+  const { container, getByText } = render(<TestRenderer />);
+  const { password, newPassword, confirmPassword } = getInputs(container);
+
+  const button = container.querySelector('button[type="submit"]');
+  await act(async () => {
+    await fireEvent.change(password, { target: { name: 'password', value: '123' } });
+    await fireEvent.change(confirmPassword, { target: { name: 'confirmPassword', value: '1235' } });
+    await fireEvent.change(newPassword, { target: { name: 'newPassword', value: '1234' } });
+  });
+
+  await act(async () => {
+    await fireEvent.click(button);
+  });
+
+  const error = getByText('Confirm password does not match new password');
+  expect(error).toBeInTheDocument();
+});
