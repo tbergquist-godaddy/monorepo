cf6e5680e5f0c1d325a077564d72e3d7acd185b6
Trond Bergquist <trond_bergquist@hotmail.com>
2020-03-15 11:22:31 +0100

    Add videostream electron app

---
+++ apps/video-stream/webpack.config.js
@@ -0,0 +1,35 @@
+// @flow
+
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new CopyPlugin([
+      {
+        from: path.join(__dirname, 'src', 'index.html'),
+        to: path.join(__dirname, 'dist', 'index.html'),
+      },
+    ]),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            rootMode: 'upward',
+          },
+        },
+      },
+    ],
+  },
+};
