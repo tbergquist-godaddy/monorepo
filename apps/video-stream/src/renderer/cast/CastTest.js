653ebec4b105f961183b56398b6d5b4145090a2d
Trond Bergquist <trond_bergquist@hotmail.com>
2020-03-21 15:10:06 +0100

    Add cast controller and use it in cast test

--- apps/video-stream/src/renderer/cast/CastTest.js
+++ apps/video-stream/src/renderer/cast/CastTest.js
@@ -4,12 +4,15 @@ import * as React from 'react';
 import { Button } from '@tbergq/components';
 import path from 'path';
 
-const startCast = electron.remote.require('./startCast').default;
+const castController = electron.remote.require('./CastController').default;
 const dialog = electron.remote.dialog;
+
+type CastState = 'idle' | 'loading' | 'casting' | 'error' | 'paused';
 
 export default function CastTest() {
   const [movie, setMovie] = React.useState(null);
   const [subtitle, setSubtitle] = React.useState(null);
+  const [castState, setCastState] = React.useState<CastState>('idle');
 
   const onSelectMovie = () => {
     const files: $ReadOnlyArray<string> | void = dialog.showOpenDialogSync({
@@ -29,8 +32,21 @@ export default function CastTest() {
       setSubtitle(files[0]);
     }
   };
-  const onStartCast = () => {
-    startCast({ movie, subtitle });
+  const onStartCast = async () => {
+    setCastState('loading');
+    try {
+      await castController.startCast({ movie, subtitle });
+      setCastState('casting');
+    } catch {
+      setCastState('error');
+    }
+  };
+  const stopCast = async () => {
+    try {
+      await castController.stopCast();
+    } finally {
+      setCastState('idle');
+    }
   };
   return (
     <>
@@ -46,7 +62,48 @@ export default function CastTest() {
         <div>Subtitle selcted: {path.basename(subtitle)}</div>
       )}
       <Button onClick={onSelectSubtitle}>Select subtitle</Button>
-      <Button onClick={onStartCast}>Start cast</Button>
+      <Button loading={castState === 'loading'} onClick={onStartCast}>
+        Start cast
+      </Button>
+      {(castState === 'casting' || castState === 'paused') && (
+        <div style={{ display: 'flex' }}>
+          <Button onClick={stopCast}>Stop</Button>
+          <Button
+            onClick={() => {
+              castController.fastForward();
+            }}
+          >
+            Fast Forward
+          </Button>
+          <Button
+            onClick={() => {
+              castController.fastRewind();
+            }}
+          >
+            Rewind
+          </Button>
+          {castState === 'casting' && (
+            <Button
+              onClick={() => {
+                castController.pause();
+                setCastState('paused');
+              }}
+            >
+              Pause
+            </Button>
+          )}
+          {castState === 'paused' && (
+            <Button
+              onClick={() => {
+                castController.resume();
+                setCastState('casting');
+              }}
+            >
+              Resume
+            </Button>
+          )}
+        </div>
+      )}
     </>
   );
 }
