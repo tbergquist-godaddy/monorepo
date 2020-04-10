3d3e575133193eeba10c514c4f1655c8265767a2
Trond Bergquist <trond_bergquist@hotmail.com>
2020-04-10 15:32:05 +0200

    use normalzie-css in withDocument

--- packages/nextjs-utils/src/withDocument.js
+++ packages/nextjs-utils/src/withDocument.js
@@ -37,10 +37,8 @@ export default function withDocument(): React.AbstractComponent<{ ... }> {
       return (
         <html lang="en">
           <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0"
-            />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="https://unpkg.com/normalize.css/" rel="stylesheet" />
             <link
               href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700"
               rel="stylesheet"
