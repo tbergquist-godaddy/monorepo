474b99b26db1c2e31fa31da8aaab752ef30d99db
Trond Bergquist <trond_bergquist@hotmail.com>
2020-06-20 10:11:40 +0200

    Change normalize css cdn url

--- packages/nextjs-utils/src/withDocument.js
+++ packages/nextjs-utils/src/withDocument.js
@@ -38,7 +38,10 @@ export default function withDocument(): React.AbstractComponent<{ ... }> {
         <html lang="en">
           <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link href="https://unpkg.com/normalize.css/" rel="stylesheet" />
+            <link
+              href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
+              rel="stylesheet"
+            />
             <link
               href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700"
               rel="stylesheet"
