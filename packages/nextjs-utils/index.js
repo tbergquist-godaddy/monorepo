123d6b67c4916e373d8275d3c4ad105c72dc6dd2
Trond Bergquist <trond_bergquist@hotmail.com>
2021-05-08 12:52:44 +0200

    next-utils: add build script

--- packages/nextjs-utils/index.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-export { default as withApp } from './src/withApp';
-export { default as withDocument } from './src/withDocument';
-export { default as useIsLoggedIn } from './src/useIsLoggedIn';
-export { default as GlobalStyle } from './src/GlobalStyle';
-export { default as ThemeProvider } from './src/ThemeProvider';
