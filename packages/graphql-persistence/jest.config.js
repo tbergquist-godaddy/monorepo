5fc92af1797f7b2e754204b6c474b4e9c32fa4c6
Trond Bergquist <trond_bergquist@hotmail.com>
2021-07-03 12:47:05 +0200

    remove mongodb-memory-server

--- packages/graphql-persistence/jest.config.js
+++ packages/graphql-persistence/jest.config.js
@@ -1,12 +1,7 @@
 // @flow
 
-const path = require('path');
-
-const setTestPath = (path.join(__dirname, 'scripts', 'setupTest.js') /*:: : string */);
-
 module.exports = {
   rootDir: __dirname,
   testEnvironment: 'node',
-  setupFilesAfterEnv: [setTestPath],
   timers: 'fake',
 };
