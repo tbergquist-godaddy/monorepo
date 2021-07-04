f2c1e2f0e6df41741bf80538113d7200c3958971
Trond Bergquist <trond_bergquist@hotmail.com>
2021-07-04 11:27:03 +0200

    gql: remove unused dataloader

--- apps/graphql/src/tvhelper/getDataloaders.ts
+++ apps/graphql/src/tvhelper/getDataloaders.ts
@@ -1,6 +1,5 @@
 import Dataloader from 'dataloader';
 
-import SearchTvShowLoader from './search/dataloaders/SearchTvShowLoader';
 import TvDetailLoader from './tvshow/dataloaders/TvDetailLoader';
 import EpisodesLoader from './episode/dataloaders/EpisodesLoader';
 import EpisodeLoader from './episode/dataloaders/EpisodeLoader';
@@ -8,14 +7,12 @@ import type { TvShow } from './tvshow/TvShow';
 import type { Episode } from './episode/Episode';
 
 export type TvHelperDataLoaders = {
-  readonly searchTvShow: Dataloader<string, TvShow[]>;
   readonly tvDetail: Dataloader<string, TvShow>;
   readonly episodes: Dataloader<string, Episode[]>;
   readonly episode: Dataloader<string, Episode>;
 };
 export default function getDataloaders(): TvHelperDataLoaders {
   return {
-    searchTvShow: SearchTvShowLoader(),
     tvDetail: TvDetailLoader(),
     episodes: EpisodesLoader(),
     episode: EpisodeLoader(),
