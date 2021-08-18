220938da220b00120073a306ac3a05a161aba666
Trond Bergquist <trond_bergquist@hotmail.com>
2021-08-18 15:51:13 +0200

    hn: setup chakra theme

---
+++ apps/handball-news/src/theme.ts
@@ -0,0 +1,21 @@
+import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const fonts = { body: `'Roboto', sans-serif` };
+
+const breakpoints = createBreakpoints({
+  sm: '576px',
+  md: '78px',
+  lg: '992px',
+  xl: '1200px',
+});
+
+const theme = extendTheme({
+  colors: {
+    black: '#16161D',
+  },
+  fonts,
+  breakpoints,
+});
+
+export default theme;
