ae2a75eb8230b30821f650a40f4258bdc78caa19
Trond Bergquist <trond_bergquist@hotmail.com>
2021-08-18 11:37:58 +0200

    hn: setup new handball-news app

---
+++ apps/handball-news/.eslintrc.js
@@ -0,0 +1,23 @@
+// @flow
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'flowtype/require-valid-file-annotation': 'off',
+    'flowtype/require-readonly-react-props': 'off',
+    'flowtype/no-types-missing-file-annotation': 'off',
+    'relay/generated-flow-types': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
+      { 'ts-expect-error': 'allow-with-description', 'ts-ignore': 'allow-with-description' },
+    ],
+    '@typescript-eslint/no-empty-function': 'off',
+    'adeira/no-invalid-flow-annotations': 'off',
+  },
+};
