dd9c849474975840d48504717dc7122519fc637a
Trond Bergquist <trond_bergquist@hotmail.com>
2021-08-18 15:51:13 +0200

    hn: setup chakra theme

---
+++ apps/handball-news/src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
+import { AppProps } from 'next/app';
+
+import theme from '../theme';
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
