9b1b72b0b5de4ff00babce71b2200f636c33ecff
Trond Bergquist <trond_bergquist@hotmail.com>
2021-08-18 16:55:48 +0200

    hn: reed 1st rss feed

--- apps/handball-news/src/theme.ts
+++ apps/handball-news/src/theme.ts
@@ -1,7 +1,11 @@
 import { extendTheme } from '@chakra-ui/react';
 import { createBreakpoints } from '@chakra-ui/theme-tools';
 
-const fonts = { body: `'Roboto', sans-serif` };
+const fonts = {
+  body: `'Roboto', sans-serif`,
+  heading: '',
+};
+fonts.heading = fonts.body;
 
 const breakpoints = createBreakpoints({
   sm: '576px',
