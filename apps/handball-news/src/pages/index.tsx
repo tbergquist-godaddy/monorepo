28be8e09c664e78df808ad93e30da8856e48831c
Trond Bergquist <trond_bergquist@hotmail.com>
2021-08-18 16:55:48 +0200

    hn: reed 1st rss feed

--- apps/handball-news/src/pages/index.tsx
+++ apps/handball-news/src/pages/index.tsx
@@ -1,14 +1,34 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Parser from 'rss-parser';
+import { Heading, Container } from '@chakra-ui/react';
+import { Trans } from 'next-i18next';
 
-export default function Home(): JSX.Element {
-  return <div>home</div>;
+export default function Home({ feed: { items } }): JSX.Element {
+  return (
+    <Container maxW="container.xl">
+      <Heading as="h1" fontSize="5xl">
+        <Trans i18nKey="common:navbar.brand">Handball news</Trans>
+      </Heading>
+      {items.map((item) => (
+        <div key={item.guid} style={{ margin: '20px 0' }}>
+          <Heading>{item.title}</Heading>
+          <div dangerouslySetInnerHTML={{ __html: item.content }} />
+        </div>
+      ))}
+    </Container>
+  );
 }
 
 export async function getStaticProps({ locale }) {
+  const parser = new Parser();
+  const [feed, translations] = await Promise.all([
+    parser.parseURL('https://www.handball-planet.com/feed/'),
+    serverSideTranslations(locale, ['common']),
+  ]);
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common'])),
-      // Will be passed to the page component as props
+      ...translations,
+      feed,
     },
   };
 }
