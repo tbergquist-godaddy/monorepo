a924c762f5466e084a871f343e44080c57525317
Trond Bergquist <trond_bergquist@hotmail.com>
2021-09-07 11:39:50 +0200

    tvh: use vanilla-extract next plugin

--- apps/tvhelper/next.config.js
+++ apps/tvhelper/next.config.js
@@ -4,45 +4,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'TRUE',
 });
 const { config } = require('dotenv');
-const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
-const { getGlobalCssLoader } = require('next/dist/build/webpack/config/blocks/css/loaders');
+const { createVanillaExtractPlugin } = require('@vanilla-extract/next-plugin');
+
+const withVanillaExtract = createVanillaExtractPlugin();
 
 config();
 
 const { GRAPHQL_URL } = process.env;
 
-function withVanillaExtract() {
-  return (nextConfig = {}) => {
-    return {
-      ...nextConfig,
-
-      webpack(config, options) {
-        const { dev, isServer } = options;
-
-        config.module.rules[config.module.rules.length - 1].oneOf.unshift({
-          test: /\.vanilla\.css$/i,
-          sideEffects: true,
-          use: getGlobalCssLoader(
-            {
-              assetPrefix: config.assetPrefix,
-              isClient: !isServer,
-              isServer,
-              isDevelopment: dev,
-            },
-            [],
-            [],
-          ),
-        });
-
-        config.plugins.push(new VanillaExtractPlugin());
-
-        return config;
-      },
-    };
-  };
-}
-
-module.exports = withVanillaExtract()(
+module.exports = withVanillaExtract(
   withBundleAnalyzer({
     images: {
       domains: ['static.tvmaze.com'],
