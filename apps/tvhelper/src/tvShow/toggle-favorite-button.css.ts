29642104605fe8136b9b901fed00ac6df3a490bf
Trond Bergquist <trond_bergquist@hotmail.com>
2021-09-23 19:19:24 +0200

    tvh: reposition toggle favorite button

--- apps/tvhelper/src/tvShow/toggle-favorite-button.css.ts
+++ apps/tvhelper/src/tvShow/toggle-favorite-button.css.ts
@@ -1,15 +1,7 @@
 import { atoms } from '@tbergq/components';
-import { style } from '@vanilla-extract/css';
 
 export const classNames = {
-  favoriteButton: style([
-    atoms({
-      position: 'absolute',
-      borderRadius: 'round',
-    }),
-    {
-      bottom: 10,
-      left: 5,
-    },
-  ]),
+  favoriteButton: atoms({
+    borderRadius: 'round',
+  }),
 };
