6ac901bff428d45c2060f0815d4a5f3ead03568f
Trond Bergquist <trond_bergquist@hotmail.com>
2021-12-08 13:17:02 +0100

    ta-gql: setup nestjs dataloader

--- apps/team-assistant-gql/src/user/user-module.ts
+++ apps/team-assistant-gql/src/user/user-module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma-module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { DataLoaderInterceptor } from '@cobraz/nestjs-dataloader';
 
+import { UserService } from './user-service';
 import { UserResolver } from './user-resolver';
+import { UserLoader } from './user-dataloader';
 
 @Module({
-  providers: [UserResolver],
+  imports: [PrismaModule],
+  providers: [
+    UserResolver,
+    UserService,
+    UserLoader,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: DataLoaderInterceptor,
+    },
+  ],
 })
 export class UserModule {}
