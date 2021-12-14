e2a1cc193007d53c38e8444b944795761f075d71
Trond Bergquist <trond_bergquist@hotmail.com>
2021-12-14 16:42:50 +0100

    ta-gql: add team connection

--- apps/team-assistant-gql/src/user/user-resolver.ts
+++ apps/team-assistant-gql/src/user/user-resolver.ts
@@ -1,10 +1,12 @@
-import { Resolver, Parent, ResolveField } from '@nestjs/graphql';
+import { Resolver, Parent, ResolveField, Args } from '@nestjs/graphql';
 import { User } from 'src/user/models/user';
 import { toGlobalId } from '@adeira/graphql-global-id';
 import { Loader } from '@cobraz/nestjs-dataloader';
 import { TeamLoader } from 'src/team/team-dataloader';
 import { Team } from 'src/team/models/team-model';
 import DataLoader from 'dataloader';
+import { GetConnectionArgs } from 'src/connection/connection-args';
+import { connectionFromPromisedArray } from 'graphql-relay';
 
 @Resolver(() => User)
 export class UserResolver {
@@ -14,7 +16,11 @@ export class UserResolver {
   }
 
   @ResolveField()
-  teams(@Parent() { id }: User, @Loader(TeamLoader) teamLoader: DataLoader<Team['id'], Team>) {
-    return teamLoader.load(id);
+  teams(
+    @Args() connectionArgs: GetConnectionArgs,
+    @Parent() { id }: User,
+    @Loader(TeamLoader) teamLoader: DataLoader<Team['id'], Array<Team>>,
+  ) {
+    return connectionFromPromisedArray<Team>(teamLoader.load(id), connectionArgs);
   }
 }
