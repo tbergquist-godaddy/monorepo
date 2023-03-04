3dbcee4e55aa9b507923ccc8cfa01ea635c961cd
Trond Bergquist <trond_bergquist@hotmail.com>
2023-03-04 13:57:15 +0100

    remove cv app
    this app has moved to its own repo

--- apps/cv/src/modules/title/title.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-type Props = {
-  children: ReactNode;
-};
-
-export default function Title({ children }: Props) {
-  return <h2 className="u-section-title">{children}</h2>;
-}
